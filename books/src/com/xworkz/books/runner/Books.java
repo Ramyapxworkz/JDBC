f67394d74bafa854224ede7c56e9edade7e28fd1
Ramyapxworkz <147068754+Ramyapxworkz@users.noreply.github.com>
2024-01-31 13:47:47 +0530

    Add files via upload

--- books/src/com/xworkz/books/runner/Books.java
+++ books/src/com/xworkz/books/runner/Books.java
@@ -5,12 +5,12 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import com.xworkz.books.constants.ConstantData;
+import com.xworkz.books.constants.ConstantBooks;
 
 public class Books {
 	public static void main(String[] args) {
-		try(Connection connection=DriverManager.getConnection(ConstantData.URL.getValue(),
-				ConstantData.USERNAME.getValue(), ConstantData.PASSWORD.getValue());
+		try(Connection connection=DriverManager.getConnection(ConstantBooks.URL.getValue(),
+				ConstantBooks.USERNAME.getValue(), ConstantBooks.PASSWORD.getValue());
 				Statement statement=connection.createStatement()){
 			System.out.println("class is connected");
 			String query="INSERT INTO book_details values('The Last Heroes','Palagummi Sainath (P Sainath)')";
---
+++ books/src/com/xworkz/books/runner/NoteBook.java
@@ -0,0 +1,30 @@
+package com.xworkz.books.runner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.xworkz.books.constants.ConstantData;
+
+public class NoteBook {		
+			public static void main(String[] args) {
+				try(Connection connection=DriverManager.getConnection(ConstantData.URL.getValue(), ConstantData.USERNAME.getValue(), ConstantData.PASSWORD.getValue());
+						Statement statement=connection.createStatement()){
+					String query="UPDATE book_details SET book_name='Durgasamana' where book_name='The Art of Programming'";
+					int rs=statement.executeUpdate(query);
+					if(rs==1) {
+						System.out.println("updated");
+					}else {
+						System.out.println("Not updated");
+					}
+				}catch(SQLException e) {
+					e.printStackTrace();
+				}
+			}
+
+		}
+
+
+
+
+
