ca57e442fb214e1d7be361fa805cf86fa647aba3
Ramyapxworkz <147068754+Ramyapxworkz@users.noreply.github.com>
2024-01-31 13:50:10 +0530

    Add files via upload

---
+++ bottle/src/com/xworkz/bottle/constants/BottleeRunner.java
@@ -0,0 +1,34 @@
+package com.xworkz.bottle.runner;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.xworkz.bottle.constants.ConstantData;
+
+public class BottleeRunner {
+	public static void main(String[] args) {
+		try (Connection connection = DriverManager.getConnection(ConstantData.URL.getValue(),
+				ConstantData.USERNAME.getValue(), ConstantData.PASSWORD.getValue());
+				Statement statement = connection.createStatement()) {
+//			System.out.println("class is connected");
+//			String query = "INSERT INTO bottle_details values('Pepsi','Plastic',750)";
+			// statement.execute(query);
+			String query="UPDATE bottle_details SET brand='Imperial' WHERE brand='Perrier'";
+			int rs = statement.executeUpdate(query);
+			if (rs == 1) {
+				System.out.println("is connected");
+			} else {
+				System.out.println("is not connectd");
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+
+		}
+	}
+
+}
+
